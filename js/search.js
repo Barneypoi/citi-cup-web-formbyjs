ddcc20c9974187ba4b4b48fb57b12b3f07f1be2f
Barneypoi <beiren.hi@163.com>
2019-07-10 10:48:29 +0800

    暂定

--- js/search.js
+++ js/search.js
@@ -1,3 +1,4 @@
+var object ,searchSequence;
 function search() {
     var xmlhttp;
     if (window.XMLHttpRequest) {
@@ -9,10 +10,27 @@ function search() {
     }
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            alert("success");
-            window.location.href = 'search.html';
+            searchSequence=xmlhttp.responseText;
+            object=JSON.parse(searchSequence);
+            var ul = document.getElementById('ul1');
+            for(i=0;i<object.length;i++){
+                var li = document.createElement("li");
+                var a = document.createElement("a");
+                ul.appendChild(li);
+                li.setAttribute("id", i + 1);
+                li.setAttribute("onclick", "getId(this)");
+                li.appendChild(a);
+                a.innerHTML = object[i].fundName;
+                function getId(obj) {
+                    //获得点击li元素的id
+                    var id = obj.id;
+                    alert("你点击的元素id是" + id);
+                }
+            }
+            // window.location.href = 'search.html';
         }
     }
     xmlhttp.open("GET", "http://47.100.120.235:8081/srcInfo?fundNameorId=" + document.getElementById("searchBox").value, true);
     xmlhttp.send();
 }
+

06ce0de9611004b08397116c8c526fd265306bbc
Barneypoi <beiren.hi@163.com>
2019-07-12 09:03:25 +0800

    收藏主功能实现

--- js/star.js
+++ js/star.js
@@ -12,35 +12,42 @@ function readstar()
             // IE6, IE5 浏览器执行代码
         xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
         }
-    xmlhttp.onreadystatechange = function ()
-    {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
-        {
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 1) {
+            alert("read1");
+        }
+        if (xmlhttp.readyState == 2) {
+            alert("read2");
+        }
+        if (xmlhttp.readyState == 3) {
+            alert("read3");
+        }
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            alert("read success");
             var myStar = xmlhttp.responseText;
             var starObject = JSON.parse(myStar);
             var sl = document.getElementById('starlist');
-            for (var j = 0; j < starObject.length; j++)
-            {
+            for (var j = 0; j < starObject.length; j++) {
                 var div = document.createElement("div");
                 var a = document.createElement("a");
                 var i = document.createElement("i");
                 sl.appendChild(div);
-                div.setAttribute("class","fund");
+                div.setAttribute("class", "fund");
                 div.appendChild(a);
-                a.setAttribute("id","chosenfund"+starObject[j].fundId);
+                a.setAttribute("id", "chosenfund" + starObject[j].fundId);
                 a.innerHTML = ("0000000000000000" + starObject[j].fundId).substr(-6);
                 div.appendChild(i);
-                i.setAttribute("class","icon fa fa-star");
-                i.setAttribute("id",starObject[j].fundId);
-                i.setAttribute("style","margin-top: 1%;margin-left:25%;font-size: 20px");
-                i.setAttribute("onclick","judgestar(this)");
-                i.setAttribute("columnNum",j);
+                i.setAttribute("class", "icon fa fa-star");
+                i.setAttribute("id", starObject[j].fundId);
+                i.setAttribute("style", "margin-top: 1%;margin-left:25%;font-size: 20px");
+                i.setAttribute("onclick", "judgestar(this)");
+                i.setAttribute("columnNum", j);
             }
         }
+    }
         xmlhttp.open("POST","http://47.100.120.235:8081/collection",true);
         xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
         xmlhttp.send("uuid=4cdfcb8a-a3b9-11e9-b20e-f42cb030f26f&operation=read&fundId=*&date="+timestamp);
-    }
 }
 
 
